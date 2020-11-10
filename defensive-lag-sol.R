4ae241d0293aa0cf0f8c0d27a5244207cf433de1
Lisa Wimmer <wml1293@outlook.de>
2020-11-10 17:16:08 +0100

    lag function done but tests not finished

--- defensive-lag-sol.R
+++ defensive-lag-sol.R
@@ -14,98 +14,49 @@ compute_lag <- function(x, n = 1L) {
   if(!test_number(n)) stop("n must be a single number")
   if(n != round(n)) stop("n must be an integer value")
   
-  base_lag <- function() {
+  base_lag <- function(x, n) {
     xlen <- length(x)
     return(c(rep(NA, n), x[seq_len(xlen - n)]))
   }
 
-  if(test_vector(x, strict = TRUE)) return(base_lag())
+  if(test_vector(x, strict = TRUE) & !(is.list(x))) {
+    return(base_lag(x = x, n = n))
+    }
   
-  warning("x is not a vector. Lagging will be applied column-wise")
+  warning("x is not a vector. Lagging is applied column-wise")
   
-  if(test_matrix(x)) apply(x, 2, base_lag)
+  if(test_matrix(x)) return(apply(x, 2, base_lag, n = n))
   
-  if(test_array(x)) {
-    array(apply(x, c(2, 3), base_lag), dim = dim(x))
+  if(test_array(x) & length(dim(x)) > 2) {
+    return(array(apply(x, c(2, 3), base_lag, n = n), dim = dim(x)))
   }
   
-  if(test_data_frame(x)) data.frame(apply(x, 2, base_lag))
+  if(test_data_frame(x)) return(data.frame(apply(x, 2, base_lag, n = n)))
   
-  if(test_list(x)) lapply(x, base_lag)
+  if(test_list(x)) return(lapply(x, base_lag, n = n))
   
-  return(NA)
-  
-}
-
-compute_lag(my_vec)
-compute_lag(my_df)
-
-compute_lag <- function(object) UseMethod("compute_lag")
-compute_lag.default <- function(object) stop("lag mi doch")
-
-compute_lag.vector <- function(x, n = 1L) {
-  
-  xlen <- length(x)
-  return(c(rep(NA, n), x[seq_len(xlen - n)]))
+  stop("x does not match any supported class")
   
 }
-
-compute_lag.matrix <- function(x, n = 1L) {
-  
-  warning("lagging will be applied to all column vectors")
-  apply(x, 2, compute_lag.vector)
-
-}
-
-compute_lag.array <- function(x, n = 1L) {
-  
-  warning("lagging will be applied to all column vectors within the enclosed 
-          matrices")
-  array(apply(x, c(2, 3), compute_lag.vector), dim = c(4, 4, 2))
-  
-}
-
-compute_lag.data.frame <- function(x, n = 1L) {
-  
-  warning("lagging will be applied to all column vectors")
-  data.frame(apply(x, 2, compute_lag.vector))
-  
-}
-
-compute_lag.list <- function(x, n = 1L) {
-  
-  warning("lagging will be applied to all list entries")
-  lapply(x, compute_lag.vector)
-  
-}
-
-my_vec <- c(1:4)
-my_list <- list(my_vec, my_vec)
-my_df <- data.frame(my_vec, my_vec)
-my_matrix <- matrix(rep(my_vec, 4), ncol = 4)
-my_array <- array(my_vec, dim = c(4, 4, 2))
-
-lag_orig <- function(x, n = 1L) {
-  xlen <- length(x)
-  c(rep(NA, n), x[seq_len(xlen - n)])
-}
-
-lag_orig(my_matrix)
-
-apply(my_matrix, 2, cumsum)
-
-z <- array(1:24, dim = 2:4)
-zseq <- apply(z, 1:2, function(x) seq_len(max(x)))
-zseq         ## a 2 x 3 matrix
-typeof(zseq) ## list
-dim(zseq) ## 2 3
-zseq[1,]
-apply(z, 3, function(x) seq_len(max(x)))
 
 # TESTS ------------------------------------------------------------------------
 
 my_vec <- c(1:4)
-my_list <- list(my_vec, my_vec)
-my_df <- data.frame(my_vec, my_vec)
 my_matrix <- matrix(rep(my_vec, 4), ncol = 4)
 my_array <- array(my_vec, dim = c(4, 4, 2))
+my_df <- data.frame(my_vec, my_vec)
+my_list <- list(my_vec, my_vec)
+my_task <- mlr3::tsk("iris")
+
+compute_lag(my_vec)
+compute_lag(my_vec, 2)
+compute_lag(my_vec, 2.5)
+compute_lag(my_vec, "2")
+compute_lag(my_vec, c(1:2))
+
+compute_lag(my_matrix)
+compute_lag(my_array)
+compute_lag(my_df)
+compute_lag(my_list)
+compute_lag(my_task)
+
